073b6086fac2a72d4da268a63cc7cc58b38297d2
timothy adigun <adigun.timothy@gmail.com>
2025-07-29 11:40:41 +0100

    integration rust test

---
+++ tests/mod.rs
@@ -0,0 +1,37 @@
+#![allow(unused, dead_code)]
+
+use arraylist::{arl::ArrayList, arraylist};
+
+#[test]
+pub fn test_arraylist_swap() {
+    let mut a = arraylist!["java", "kotlin"];
+    a.swap(0, 1);
+    assert_eq!(a, arraylist!("kotlin", "java"));
+}
+
+#[test]
+pub fn test_arraylist_swap_comparing_using_numbers() {
+    let a = arraylist![3, 9, 0, -1, 4, 5, 10, -3];
+    for _ in 0..a.len() {
+        for j in 0..a.len() - 1 {
+            if a.get(j).unwrap() > a.get(j + 1).unwrap() {
+                a.swap(j, j + 1);
+            }
+        }
+    }
+    assert_eq!(a.to_vec(), arraylist!(-3, -1, 0, 3, 4, 5, 9, 10).to_vec());
+}
+
+#[test]
+#[ignore = "todo"]
+pub fn test_arraylist_swap_comparing() {
+    let a = arraylist!["java", "kotlin", "clojure"];
+    for _ in 0..a.len() {
+        for j in 0..a.len() - 1 {
+            if a.get(j).unwrap() > a.get(j + 1).unwrap() {
+                a.swap(j, j + 1);
+            }
+        }
+    }
+    assert_eq!(a.to_vec(), arraylist!("clojure", "kotlin", "java").to_vec());
+}
